e55d751bd88ca0b87de479f82e8c440faf04db54
Fqah <osmk@qq.com>
2016-04-18 15:55:05 +0800

    first

---
+++ JSPatchDemo/JPViewController.h
@@ -0,0 +1,15 @@
+//
+//   /\_/\
+//   \_ _/
+//    / \ not
+//    \_/
+//
+//  Created by __无邪_ on 4/18/16.
+//  Copyright © 2016 fqah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JPViewController: UIViewController
+
+@end
---
+++ JSPatchDemo/JPViewController.m
@@ -0,0 +1,56 @@
+//
+//   /\_/\
+//   \_ _/
+//    / \ not
+//    \_/
+//
+//  Created by __无邪_ on 4/18/16.
+//  Copyright © 2016 fqah. All rights reserved.
+//
+
+#import "JPViewController.h"
+
+@interface JPViewController ()
+
+@end
+
+@implementation JPViewController
+
+#pragma mark - LifeCircle
+
+- (void)initModel{
+    // 初始化Model
+    
+}
+- (void)initView{
+    // 初始化View
+    
+}
+- (void)initData{
+    // 初始化Data
+    
+}
+
+#pragma mark - Network Methods
+
+#pragma mark - Action
+
+#pragma mark - Delegate
+
+#pragma mark - Private
+
+#pragma mark - Configure
+
+
+
+
+
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
