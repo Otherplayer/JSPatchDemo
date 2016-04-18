de5ab3ca19f2ba9c05513d5aaeeb3223a5bf9bcb
Fqah <osmk@qq.com>
2016-04-18 15:34:28 +0800

    Initial Commit

---
+++ JSPatchDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JSPatchDemo
+//
+//  Created by __无邪_ on 4/18/16.
+//  Copyright © 2016 fqah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JSPatchDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  JSPatchDemo
+//
+//  Created by __无邪_ on 4/18/16.
+//  Copyright © 2016 fqah. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
